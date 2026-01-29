77aef03b222982f96d7ca0d26d123fe03cb34b64
Jose David Carranza Angarita <ing.josedavidcarranzaangarita@gmail.com>
2026-01-29 09:50:05 -0500

    Sección 1: Hola Mundo finalizada

---
+++ src/com/LearningJava/HolaMundo/_01_HolaMundo.java
@@ -0,0 +1,8 @@
+package com.LearningJava.HolaMundo;
+
+public class _01_HolaMundo {
+    public static void main(String[] args) {
+        // Clase System con su atributo out y uso del metodo print para mostrar en pantalla
+        System.out.print("\n>> Hola Mundo Desde Java con IA - Insomio y Ansiedad¸\n");
+    }
+}
---
+++ src/com/LearningJava/HolaMundo/_02_Ejercicio1.java
@@ -0,0 +1,28 @@
+package com.LearningJava.HolaMundo;
+
+public class _02_Ejercicio1 {
+    public static void main(String[] args) {
+        //Escribir su presentación con:
+
+        //Nombre completo , edad y país.
+
+
+        //Usamos salida normal
+
+        System.out.println("\nMi nombre es : Jose David Carranza angarita");
+        System.out.println("Tengo 20 años y soy del País de Colombia");
+
+
+        //Usamos el textBlock
+
+        System.out.printf("""
+                
+                ------------------------------------------------
+                Hola mi nombre es: Jose David Carranza Angarita
+                tengo 20 años y soy del país de Colombia
+                
+                Colombia 2026
+                """
+        );
+    }
+}
