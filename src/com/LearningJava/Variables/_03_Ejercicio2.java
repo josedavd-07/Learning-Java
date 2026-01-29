ed126047ae0193e6242a2b95e6889442ae7135e2
Jose David Carranza Angarita <ing.josedavidcarranzaangarita@gmail.com>
2026-01-29 09:54:14 -0500

    Sección 2: Capitulo 3 ejercicios variables

---
+++ src/com/LearningJava/Variables/_03_Ejercicio2.java
@@ -0,0 +1,33 @@
+package com.LearningJava.Variables;
+
+public class _03_Ejercicio2 {
+    public static void main(String[] args) {
+        //Escriba los datos de un libro : Name, age, country, height y status - S = Soltero   - C = Casado
+
+        String name = "Cien años de soledad";
+
+        int age = 20;
+
+        String country = "Colombia";
+
+        double height = 177;
+
+        char status = 'S';
+
+        System.out.printf("""
+                
+                ---------- Presentation ----------
+                Book: %s
+                Age: %d
+                Country: %s
+                Height: %.0f cm
+                Status: %s
+                """,
+                name,
+                age,
+                country,
+                height,
+                status
+        );
+    }
+}
